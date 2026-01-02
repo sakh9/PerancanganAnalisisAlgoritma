56c1afe57736725e97148149e2bd7e6b74385e91
sakha <163008703+sakh9@users.noreply.github.com>
2026-01-02 20:55:11 +0700

    Add climbStairs function to calculate ways to climb
    Implement climbing stairs function with dynamic programming.

---
+++ Tugas 13/ClimbingStairs.py
@@ -0,0 +1,16 @@
+def climbStairs(n):
+
+    f = [0] * (n + 1)
+    f[1] = 1
+    f[2] = 2
+
+    for i in range(3, n + 1):
+        
+        f[i] = f[i - 1] + f[i - 2]
+        print(f"Iterasi i = {i} → f[{i}] = f[{i-1}] + f[{i-2}]")
+        print(f)
+    return f[n]
+
+# contoh penggunaan
+n = int(input("Masukkan jumlah tangga: "))   
+print("Jumlah cara untuk mendaki", n, "tangga adalah:", climbStairs(n))
